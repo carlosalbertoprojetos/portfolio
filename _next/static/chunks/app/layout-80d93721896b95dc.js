a6a5f1ca771ebe0633c82244bfe8a66d04d6d857
Carlos Alberto <carlosalbertoprojetos2020@gmail.com>
2026-06-29 16:17:40 -0300

    Updates

---
+++ _next/static/chunks/app/layout-80d93721896b95dc.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{428:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,3673,23))},3673:()=>{}},e=>{e.O(0,[978,441,255,358],()=>e(e.s=428)),_N_E=e.O()}]);
